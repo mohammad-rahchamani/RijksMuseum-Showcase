5fd1c90d06e87e273286c160f8cf288e849ad5c6
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-24 14:19:49 +0430

    Initial Commit

---
+++ RijksMuseum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RijksMuseum
+//
+//  Created by Mohammad Rahchamani on 4/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RijksMuseum/RijksMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  RijksMuseumApp.swift
+//  RijksMuseum
+//
+//  Created by Mohammad Rahchamani on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RijksMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
