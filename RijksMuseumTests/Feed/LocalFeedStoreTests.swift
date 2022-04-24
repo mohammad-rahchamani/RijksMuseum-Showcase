afcec4fef788f9c33c0b85be393b2a4cfbe0ad10
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-24 18:27:18 +0430

    side effects run serially to avoid race conditions after moving data parsing to background thread

--- RijksMuseumTests/Feed/LocalFeedStoreTests.swift
+++ RijksMuseumTests/Feed/LocalFeedStoreTests.swift
@@ -21,6 +21,9 @@ class LocalFeedStore {
     }
     
     private let storeURL: URL
+    private let queue = DispatchQueue(label: "\(type(of: LocalFeedStore.self))",
+                                      qos: .background,
+                                      attributes: .concurrent)
     
     init(storeURL: URL) {
         self.storeURL = storeURL
@@ -33,41 +36,47 @@ class LocalFeedStore {
     }
     
     func load(completion: @escaping (Result<LoadResult, Error>) -> Void) {
-        
-        do {
-            let data = try Data(contentsOf: storeURL)
-            guard !data.isEmpty else {
-                completion(.success(.empty))
-                return
+        let storeURL = storeURL
+        queue.async {
+            do {
+                let data = try Data(contentsOf: storeURL)
+                guard !data.isEmpty else {
+                    completion(.success(.empty))
+                    return
+                }
+                let decoder = JSONDecoder()
+                let parsedData = try decoder.decode(DataRepresentation.self, from: data)
+                completion(.success(.result(parsedData)))
+            } catch {
+                completion(.failure(error))
             }
-            let decoder = JSONDecoder()
-            let parsedData = try decoder.decode(DataRepresentation.self, from: data)
-            completion(.success(.result(parsedData)))
-        } catch {
-            completion(.failure(error))
         }
-        
-        
     }
     
     func save(data: DataRepresentation,
               completion: @escaping (Result<Void, Error>) -> Void) {
-        do {
-            let encoder = JSONEncoder()
-            let encodedData = try encoder.encode(data)
-            try encodedData.write(to: storeURL)
-            completion(.success(()))
-        } catch {
-            completion(.failure(error))
+        let storeURL = storeURL
+        queue.async(flags: .barrier) {
+            do {
+                let encoder = JSONEncoder()
+                let encodedData = try encoder.encode(data)
+                try encodedData.write(to: storeURL)
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
         }
     }
     
     func delete(completion: @escaping (Result<Void, Error>) -> Void) {
-        do {
-            try Data().write(to: storeURL)
-            completion(.success(()))
-        } catch {
-            completion(.failure(error))
+        let storeURL = storeURL
+        queue.async(flags: .barrier) {
+            do {
+                try Data().write(to: storeURL)
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
         }
     }
     
@@ -113,18 +122,14 @@ class LocalFeedStoreTests: XCTestCase {
     
     func test_load_deliversDataFromStore() {
         let sut = makeSUT(storeURL: storeURLForTest())
-        let feed = [anyFeedItem(), anyFeedItem(), anyFeedItem()]
-        let timestamp = Date()
-        let expectedResult = LocalFeedStore.DataRepresentation(feed: feed, timestamp: timestamp)
+        let expectedResult = anyDataRepresentation()
         save(expectedResult, to: sut)
         expect(sut, toCompleteLoadWith: .success(.result(expectedResult)))
     }
     
     func test_load_hasNoSideEffectsOnStoreWithData() {
         let sut = makeSUT(storeURL: storeURLForTest())
-        let feed = [anyFeedItem(), anyFeedItem(), anyFeedItem()]
-        let timestamp = Date()
-        let expectedResult = LocalFeedStore.DataRepresentation(feed: feed, timestamp: timestamp)
+        let expectedResult = anyDataRepresentation()
         save(expectedResult, to: sut)
         expect(sut, toCompleteLoadWith: .success(.result(expectedResult)))
         expect(sut, toCompleteLoadWith: .success(.result(expectedResult)))
@@ -132,9 +137,7 @@ class LocalFeedStoreTests: XCTestCase {
     
     func test_save_completesSuccessfullyOnValidDataAndValidStoreURL() {
         let sut = makeSUT(storeURL: storeURLForTest())
-        let feed = [anyFeedItem(), anyFeedItem(), anyFeedItem()]
-        let timestamp = Date()
-        let expectedResult = LocalFeedStore.DataRepresentation(feed: feed, timestamp: timestamp)
+        let expectedResult = anyDataRepresentation()
         expect(sut,
                toSave: expectedResult,
                andCompleteSaveWith: .success(()))
@@ -161,8 +164,7 @@ class LocalFeedStoreTests: XCTestCase {
     
     func test_save_failsOnStoreError() {
         let sut = makeSUT(storeURL: invalidStoreURL())
-        let data = LocalFeedStore.DataRepresentation(feed: [anyFeedItem(), anyFeedItem()],
-                                                     timestamp: Date())
+        let data = anyDataRepresentation()
         expect(sut, toSave: data, andCompleteSaveWith: .failure(anyNSError()))
     }
     
@@ -180,8 +182,40 @@ class LocalFeedStoreTests: XCTestCase {
         let sut = makeSUT(storeURL: storeURLForTest())
         let data = LocalFeedStore.DataRepresentation(feed: [anyFeedItem()], timestamp: Date())
         save(data, to: sut)
-        sut.delete { _ in }
+        delete(sut)
         expect(sut, toCompleteLoadWith: .success(.empty))
+    }
+    
+    func test_sideEffects_runSeriallyToAvoidRaceConditions() {
+        let sut = makeSUT(storeURL: storeURLForTest())
+        let op1 = XCTestExpectation(description: "waiting for operation 1")
+        let op2 = XCTestExpectation(description: "waiting for operation 2")
+        let op3 = XCTestExpectation(description: "waiting for operation 3")
+        let op4 = XCTestExpectation(description: "waiting for operation 4")
+        let op5 = XCTestExpectation(description: "waiting for operation 5")
+        var completedOperations: [XCTestExpectation] = []
+        sut.save(data: anyDataRepresentation()) { _ in
+            completedOperations.append(op1)
+            op1.fulfill()
+        }
+        sut.delete { _ in
+            completedOperations.append(op2)
+            op2.fulfill()
+        }
+        sut.save(data: anyDataRepresentation()) { _ in
+            completedOperations.append(op3)
+            op3.fulfill()
+        }
+        sut.delete { _ in
+            completedOperations.append(op4)
+            op4.fulfill()
+        }
+        sut.save(data: anyDataRepresentation()) { _ in
+            completedOperations.append(op5)
+            op5.fulfill()
+        }
+        wait(for: [op1, op2, op3, op4, op5], timeout: 1)
+        XCTAssertEqual(completedOperations, [op1, op2, op3, op4, op5], "expected op1, op2, op3, op4, op5 in order, got \(completedOperations) instead.")
     }
     
     // MARK: helpers
@@ -253,7 +287,19 @@ class LocalFeedStoreTests: XCTestCase {
     }
     
     func save(_ data: LocalFeedStore.DataRepresentation, to store: LocalFeedStore) {
-        store.save(data: data) { _ in }
+        let exp = XCTestExpectation(description: "waiting for save completion")
+        store.save(data: data) { _ in
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func delete(_ store: LocalFeedStore) {
+        let exp = XCTestExpectation(description: "waiting for deolete completion")
+        store.delete { _ in
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
     
     func storeURLForTest() -> URL {
@@ -261,6 +307,10 @@ class LocalFeedStoreTests: XCTestCase {
             .urls(for: .cachesDirectory, in: .userDomainMask)
             .first!
             .appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    func anyDataRepresentation() -> LocalFeedStore.DataRepresentation {
+        LocalFeedStore.DataRepresentation(feed: [anyFeedItem(), anyFeedItem()], timestamp: Date())
     }
     
     func invalidStoreURL() -> URL {
