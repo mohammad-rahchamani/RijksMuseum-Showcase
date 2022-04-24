242fcd52e2119ada5aa2b8c81fbefb769a07281c
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-24 17:18:43 +0430

    move shared helpers to an extension

---
+++ RijksMuseumTests/Helper/XCTestCase+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  XCTestCase+Extension.swift
+//  RijksMuseumTests
+//
+//  Created by Mohammad Rahchamani on 4/24/22.
+//
+
+import XCTest
+import RijksMuseum
+
+public extension XCTestCase {
+    
+    func invalidData() -> Data {
+        "invalid data".data(using: .utf8)!
+    }
+    
+    func anyFeedImage() -> FeedImage {
+        FeedImage(guid: "guid", url: "url string")
+    }
+    
+    func anyFeedItem() -> FeedItem {
+        FeedItem(id: "id",
+                 objectNumber: "object number",
+                 title: "title",
+                 longTitle: "long title",
+                 webImage: anyFeedImage(),
+                 headerImage: anyFeedImage())
+    }
+    
+    func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
+    }
+    
+    func anyNSError() -> NSError {
+        NSError(domain: "any error", code: 1, userInfo: nil)
+    }
+    
+    func getData(from data: [FeedItem]) -> Data {
+        RemoteFeedLoader.remoteRepresentaionData(for: data)!
+    }
+}
