6647adcdf23e1deb56d401a529e4c399d9343ec1
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-24 17:19:33 +0430

    local feed store creates a new file for store load has no side effects on empty store and store error

--- RijksMuseumTests/Feed/LocalFeedStoreTests.swift
+++ RijksMuseumTests/Feed/LocalFeedStoreTests.swift
@@ -6,30 +6,142 @@
 //
 
 import XCTest
+import RijksMuseum
+
+class LocalFeedStore {
+    
+    typealias DataRepresentation = (feed: [FeedItem], timestamp: Date)
+    
+    enum LoadResult: Equatable {
+        static func == (lhs: LocalFeedStore.LoadResult, rhs: LocalFeedStore.LoadResult) -> Bool {
+            switch (lhs, rhs) {
+            case (.empty, .empty):
+                return true
+            case (.result(let lData), .result(let rData)):
+                return lData.feed == rData.feed && lData.timestamp == rData.timestamp
+            default:
+                return false
+            }
+        }
+        
+        case empty
+        case result(DataRepresentation)
+    }
+    
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+        self.setupStore()
+    }
+    
+    private func setupStore() {
+        guard !FileManager.default.fileExists(atPath: storeURL.path) else { return }
+        try? "".data(using: .utf8)?.write(to: storeURL)
+    }
+    
+    func load(completion: @escaping (Result<LoadResult, Error>) -> Void) {
+        do {
+            _ = try Data(contentsOf: storeURL)
+            completion(.success(.empty))
+        } catch {
+            completion(.failure(error))
+        }
+        
+        
+    }
+    
+    func save(data: DataRepresentation,
+              completion: @escaping (Result<Void, Error>) -> Void) {
+        
+    }
+    
+    func delete(completion: @escaping (Result<Void, Error>) -> Void) {
+        
+    }
+    
+    
+}
 
 class LocalFeedStoreTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        removeStoreFile()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        removeStoreFile()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_init_createsFileForStore() {
+        let _ = makeSUT(storeURL: storeURLForTest())
+        XCTAssertTrue(FileManager.default.fileExists(atPath: storeURLForTest().path), "expected a file at store path: \(storeURLForTest().path).")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_load_returnsEmptyInitially() {
+        let sut = LocalFeedStore(storeURL: storeURLForTest())
+        expect(sut, toCompleteLoadWith: .success(.empty))
+    }
+    
+    func test_load_hasNoSideEffectsOnEmptyStore() {
+        let sut = makeSUT(storeURL: storeURLForTest())
+        expect(sut, toCompleteLoadWith: .success(.empty))
+        expect(sut, toCompleteLoadWith: .success(.empty))
+    }
+    
+    func test_load_returnsErrorOnStoreError() {
+        let sut = makeSUT(storeURL: invalidStoreURL())
+        expect(sut, toCompleteLoadWith: .failure(anyNSError()))
+    }
+    
+    func test_load_hasNoSideEffectsOnStoreError() {
+        let sut = makeSUT(storeURL: invalidStoreURL())
+        expect(sut, toCompleteLoadWith: .failure(anyNSError()))
+        expect(sut, toCompleteLoadWith: .failure(anyNSError()))
+    }
+    
+    // MARK: helpers
+    
+    func makeSUT(storeURL: URL, file: StaticString = #file, line: UInt = #line) -> LocalFeedStore {
+        let sut = LocalFeedStore(storeURL: storeURL)
+        addTeardownBlock { [weak sut] in
+            XCTAssertNil(sut, "sut should be deallocated", file: file, line: line)
         }
+        return sut
+    }
+    
+    func expect(_ sut: LocalFeedStore,
+                toCompleteLoadWith expectedResult: Result<LocalFeedStore.LoadResult, Error>,
+                file: StaticString = #file,
+                line: UInt = #line) {
+        let exp = XCTestExpectation(description: "waiting for load completion")
+        sut.load { capturedResult in
+            switch (capturedResult, expectedResult) {
+            case (.failure, .failure):
+                ()
+            case (.success(let capturedValue),.success(let expectedValue)):
+                XCTAssertEqual(capturedValue, expectedValue, file: file, line: line)
+            default:
+                XCTFail("expected \(expectedResult) got \(capturedResult) instead.", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func storeURLForTest() -> URL {
+        FileManager.default
+            .urls(for: .cachesDirectory, in: .userDomainMask)
+            .first!
+            .appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    func invalidStoreURL() -> URL {
+        anyURL()
+    }
+    
+    func removeStoreFile() {
+        try? FileManager.default.removeItem(at: storeURLForTest())
     }
 
 }
