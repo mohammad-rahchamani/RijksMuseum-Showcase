9a0a6c27fb74fbb8d13b2f3256887e21e809d7ad
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-24 21:51:27 +0430

    add feed store spy to test feed cache interactions with feed store feed cache requests load from feed store on load

--- RijksMuseumTests/Feed/FeedCacheTests.swift
+++ RijksMuseumTests/Feed/FeedCacheTests.swift
@@ -8,29 +8,117 @@
 import XCTest
 import RijksMuseum
 
+class FeedStoreSpy: FeedStore {
+    
+    private(set) var messages: [Message] = []
+    
+    private var loadCompletions: [(Result<FeedStoreResult, Error>) -> Void] = []
+    private var saveCompletions: [(Result<Void, Error>) -> Void] = []
+    private var deleteCompletions: [(Result<Void, Error>) -> Void] = []
+    
+    enum Message: Equatable {
+        case load
+        case save(FeedStoreDataRepresentation)
+        case delete
+    }
+    
+    func resetMessages() {
+        messages.removeAll()
+    }
+    
+    func completeLoad(at index: Int = 0,
+                      withResult result: Result<FeedStoreResult, Error>) {
+        loadCompletions[index](result)
+    }
+    
+    func completeSave(at index: Int = 0,
+                      withResult result: Result<Void, Error>) {
+        saveCompletions[index](result)
+    }
+    
+    func completeDelete(at index: Int = 0,
+                        withResult result: Result<Void, Error>) {
+        deleteCompletions[index](result)
+    }
+    
+    func load(completion: @escaping (Result<FeedStoreResult, Error>) -> Void) {
+        messages.append(.load)
+    }
+    
+    func save(data: FeedStoreDataRepresentation, completion: @escaping (Result<Void, Error>) -> Void) {
+        messages.append(.save(data))
+    }
+    
+    func delete(completion: @escaping (Result<Void, Error>) -> Void) {
+        messages.append(.delete)
+    }
+    
+    deinit {
+        loadCompletions = []
+        saveCompletions = []
+        deleteCompletions = []
+        messages = []
+    }
+    
+}
+
+class FeedCache {
+    
+    let store: FeedStore
+    let remoteLoader: RemoteFeedLoader
+    
+    init(store: FeedStore, remoteLoader: RemoteFeedLoader) {
+        self.store = store
+        self.remoteLoader = remoteLoader
+    }
+    
+    func load(completion: @escaping (Result<[FeedItem], Error>) -> Void) {
+        self.store.load { _ in
+            completion(.success([]))
+        }
+    }
+    
+}
+
+
+
 class FeedCacheTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        URLProtocolStub.startIntercepting()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        URLProtocolStub.stopIntercepting()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_init_doesNotMessageStoreAndNetwork() {
+        var networkCallCount = 0
+        URLProtocolStub.observe { _ in
+            networkCallCount += 1
         }
+        let (_, spy) = makeSUT()
+        XCTAssertEqual(networkCallCount, 0, "expected 0 network calls, got \(networkCallCount) instead.")
+        XCTAssertTrue(spy.messages.isEmpty, "expected no messages to feed store, got \(spy.messages) instead.")
+    }
+    
+    func test_load_requestsLoadFromLocalStorage() {
+        let (sut, spy) = makeSUT()
+        sut.load() { _ in }
+        XCTAssertEqual(spy.messages, [.load], "expected load message feed store, got \(spy.messages) instead.")
+    }
+    
+    // MARK: helpers
+    
+    func makeSUT(file: StaticString = #file,
+                 line: UInt = #line) -> (FeedCache, FeedStoreSpy) {
+        let spy = FeedStoreSpy()
+        let remoteLoader = RemoteFeedLoader(session: .shared)
+        let sut = FeedCache(store: spy, remoteLoader: remoteLoader)
+        trackForMemoryLeaks(spy, file: file, line: line)
+        trackForMemoryLeaks(remoteLoader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, spy)
     }
 
 }
