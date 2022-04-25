b824af3d163d6c629c2824dfdaf048f19b905758
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-25 08:29:31 +0430

    add dynamic image view and view model to load image using a url

--- RijksMuseum/Component/DynamicImage/DynamicImageView.swift
+++ RijksMuseum/Component/DynamicImage/DynamicImageView.swift
@@ -7,52 +7,6 @@
 
 import SwiftUI
 
-
-class DynamicImageViewModel: ObservableObject {
-    
-    @Published var image: UIImage?
-    
-    private let imageLoader: ImageLoader
-    private let imageMapper: ImageMapper
-    
-    init(loader: ImageLoader, mapper: ImageMapper) {
-        self.imageLoader = loader
-        self.imageMapper = mapper
-    }
-    
-    func load(from url: URL) {
-        imageLoader.load(from: url) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .failure:
-                ()
-            case .success(let imageData):
-                self.map(imageData)
-            }
-        }
-    }
-    
-    private func map(_ data: Data) {
-        self.imageMapper.map(data: data) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .failure:
-                ()
-            case .success(let image):
-                self.image = image
-            }
-        }
-    }
-    
-}
-
-extension DynamicImageViewModel {
-    static var preview: DynamicImageViewModel {
-        DynamicImageViewModel(loader: ImageLoader(cache: URLCache()),
-                              mapper: SimpleImageMapper())
-    }
-}
-
 struct DynamicImageView<PlaceHolder: View>: View {
     
     @StateObject var viewModel: DynamicImageViewModel
