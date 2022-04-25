5aab1384fc46aac99edca0c6c7bf37a8c9f58644
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-25 09:32:42 +0430

    update dynamic image view to create a view model with same life cycle as view (state object)

--- RijksMuseum/Component/DynamicImage/DynamicImageView.swift
+++ RijksMuseum/Component/DynamicImage/DynamicImageView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct DynamicImageView<PlaceHolder: View>: View {
     
     @StateObject var viewModel: DynamicImageViewModel
-    private let url: URL
     private let placerHolder: () -> PlaceHolder
     
-    init(viewModel: DynamicImageViewModel,
+    init(loader: ImageLoader,
+         mapper: ImageMapper,
          url: URL,
          @ViewBuilder placeHolder: @escaping () -> PlaceHolder) {
-        self._viewModel = StateObject(wrappedValue: viewModel)
-        self.url = url
+        self._viewModel = StateObject(wrappedValue: DynamicImageViewModel(loader: loader,
+                                                                          mapper: mapper,
+                                                                          url: url))
+
         self.placerHolder = placeHolder
     }
     
@@ -35,14 +37,16 @@ struct DynamicImageView<PlaceHolder: View>: View {
     var body: some View {
         imageView()
             .onAppear {
-                self.viewModel.load(from: self.url)
+                self.viewModel.load()
             }
     }
 }
 
 struct DynamicImageView_Previews: PreviewProvider {
     static var previews: some View {
-        DynamicImageView(viewModel: .preview,
+        
+        DynamicImageView(loader: ImageLoader.init(cache: URLCache()),
+                         mapper: SimpleImageMapper(),
                          url: URL(string: "https://lh3.googleusercontent.com/gShVRyvLLbwVB8jeIPghCXgr96wxTHaM4zqfmxIWRsUpMhMn38PwuUU13o1mXQzLMt5HFqX761u8Tgo4L_JG1XLATvw=s0")!,
                          placeHolder: {
             Image(systemName: "trash")
--- RijksMuseum/Component/DynamicImage/DynamicImageViewModel.swift
+++ RijksMuseum/Component/DynamicImage/DynamicImageViewModel.swift
@@ -14,13 +14,15 @@ class DynamicImageViewModel: ObservableObject {
     
     private let imageLoader: ImageLoader
     private let imageMapper: ImageMapper
+    private let url: URL
     
-    init(loader: ImageLoader, mapper: ImageMapper) {
+    init(loader: ImageLoader, mapper: ImageMapper, url: URL) {
         self.imageLoader = loader
         self.imageMapper = mapper
+        self.url = url
     }
     
-    func load(from url: URL) {
+    func load() {
         imageLoader.load(from: url) { [weak self] result in
             guard let self = self else { return }
             switch result {
@@ -33,7 +35,9 @@ class DynamicImageViewModel: ObservableObject {
     }
     
     private func map(_ data: Data) {
+        print("DYN MAP")
         self.imageMapper.map(data: data) { [weak self] result in
+            print("DYN MAP Done")
             guard let self = self else { return }
             switch result {
             case .failure:
@@ -49,6 +53,7 @@ class DynamicImageViewModel: ObservableObject {
 extension DynamicImageViewModel {
     static var preview: DynamicImageViewModel {
         DynamicImageViewModel(loader: ImageLoader(cache: URLCache()),
-                              mapper: SimpleImageMapper())
+                              mapper: SimpleImageMapper(),
+                              url: URL(string: "https://any-url.com")!)
     }
 }
