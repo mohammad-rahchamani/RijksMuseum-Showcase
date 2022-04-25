d3e3dd23e6939e4242584d6733c08a84e51c6a71
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-25 08:47:55 +0430

    add feed list view model to handle loading and selection state of view

--- RijksMuseum/UI/FeedListView/FeedListViewModel.swift
+++ RijksMuseum/UI/FeedListView/FeedListViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class FeedListViewModel: ObservableObject {
+    
+    @Published var isLoading: Bool = false
+    @Published var feed: [FeedItem] = []
+    @Published var loadFailed: Bool = false
+    @Published var selectedItemId: String? = nil
+    
+    private let loader: FeedLoader
+    
+    init(loader: FeedLoader) {
+        self.loader = loader
+    }
+    
+    func load() {
+        self.loadFailed = false
+        self.isLoading = true
+        self.loader.load { result in
+            self.isLoading = false
+            switch result {
+            case .success(let items):
+                self.feed = items
+            case .failure:
+                self.loadFailed = true
+            }
+        }
+    }
+    
+}
+
+extension FeedListViewModel {
+    static var preview: FeedListViewModel {
+        FeedListViewModel(loader: FeedLoaderStub())
+    }
+}
