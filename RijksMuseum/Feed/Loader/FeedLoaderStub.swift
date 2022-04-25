3572f0fb105349a69a1f90146aa611674941d41a
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-25 08:46:48 +0430

    add feed loader stub for preview

--- RijksMuseum/Feed/Loader/FeedLoaderStub.swift
+++ RijksMuseum/Feed/Loader/FeedLoaderStub.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class FeedLoaderStub: FeedLoader {
+    
+    private let items: [FeedItem]
+    
+    init(items: [FeedItem] = [FeedItem(id: "id",
+                                       objectNumber: "number",
+                                       title: "title",
+                                       longTitle: "long title",
+                                       webImage: FeedImage(guid: "", url: ""),
+                                       headerImage: FeedImage(guid: "", url: ""))]) {
+        self.items = items
+    }
+    
+    func load(completion: @escaping (Result<[FeedItem], Error>) -> Void) {
+        completion(.success(items))
+    }
+}
