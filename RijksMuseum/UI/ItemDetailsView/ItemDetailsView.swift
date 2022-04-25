74b25f72220018dc9b0360a3c9ac159748e20343
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-25 09:40:52 +0430

    create new file for item detail view

---
+++ RijksMuseum/UI/ItemDetailsView/ItemDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetailsView.swift
+//  RijksMuseum
+//
+//  Created by Mohammad Rahchamani on 4/25/22.
+//
+
+import SwiftUI
+
+struct ItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetailsView()
+    }
+}
